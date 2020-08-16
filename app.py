0469e784dea7b4981c8520a61beb70b503f8a2e9
mdb571 <r.mohammedbilal@gmail.com>
2020-08-16 13:45:08 +0530

    new event

--- app.py
+++ app.py
@@ -19,27 +19,36 @@ if num is None:
 # ---------- Checking for responses ---------------
 
 if event == "pull_request":
-    github_url = f'https://github.com/{repo}/pulls/{num}'
+    repo_url = f'https://github.com/{repo}/pulls/{num}'
     if action == "opened":
-        response = f'A new Pull request *{title}* (*#{num}*) has been opened in *{repo}* by *{name}*  \n\n {github_url}'
+        response = f'A new Pull request *{title}* *#{num}* has been opened in *{repo}* by *{name}*  \n\n {repo_url}'
         
     elif action == 'closed':
-        response = f'The Pull request *{title}* (#{num}) on *{repo}* has been closed by *{name}*, \n\n {github_url}'
-        elif event == 'issues':
-    github_url = f'https://github.com/{repo}/issues/{num}'
+        response = f'The Pull request *{title}* *#{num}* on *{repo}* has been closed by *{name}*, \n\n {repo_url}'
+
+elif event == 'issues':
+    repo_url = f'https://github.com/{repo}/issues/{num}'
     if action == 'opened':
-        response = f'A new Issue *{title}* (*#{num}*) has been opened in *{repo}* by *{name}* \n\n {github_url}'
+        response = f'A new Issue *{title}* *#{num}* has been opened in *{repo}* by *{name}* \n\n {repo_url}'
         
     elif action == 'closed':
-        response = f'The Issue (*#{num}*) on *{repo}* was closed by *{name}*, \n\n {github_url}'
+        response = f'The Issue *#{num}* on *{repo}* was closed by *{name}*, \n\n {repo_url}'
+
+     else:
+         response = f'A new {action} event was triggered on the issue *{title}* *#{num}* on *{repo}*  by *{name}*, \n\n {repo_url}'
+elif event=='page_build':
+    page_url='https://'+name+'.github.io/'+repo
+    response = f'A new page build was triggered on the  on *{repo}*  by *{name}*, \n\n {page_url}'
 
 else:
-    github_url = f'https://github.com/{repo}'
-    response = f'A new *{event}* has been made in *{repo}* by *{name}*, \n\n {github_url}'
+    repo_url = f'https://github.com/{repo}'
+    response = f'A new *{event}* has been made in *{repo}* by *{name}*, \n\n {repo_url}'
 
 
 #creating twilio client
 
+From='whatsapp:'+From
+To='whatsapp:'+To
 client=Client(account_sid,authtoken)
 message = client.messages \
     .create(
